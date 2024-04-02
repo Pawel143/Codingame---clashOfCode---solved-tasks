53905bd048a66b22d2f320c483c65ed39470a4a2
Paweł Knapik <btc159357@gmail.com>
2024-04-02 22:19:35 +0200

    Tasks from 02.04.2024

---
+++ src/main/java/Tasks/FastestMode1/FM.java
@@ -0,0 +1,33 @@
+package Tasks.FastestMode1;
+
+import java.util.Scanner;
+
+public class FM {
+
+    /**
+     * Auto-generated code below aims at helping you parse
+     * the standard input according to the problem statement.
+     **/
+    class Solution {
+
+        public void task() {
+            Scanner in = new Scanner(System.in);
+            StringBuilder n = new StringBuilder(in.nextLine());
+
+            char[] arr = n.toString().toCharArray();
+            n = new StringBuilder();
+            for (char x : arr) {
+                if (x % 2 == 0) {
+                    n.append("#");
+                } else {
+                    n.append("*");
+                }
+            }
+
+            // Write an answer using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println(n);
+        }
+    }
+}
