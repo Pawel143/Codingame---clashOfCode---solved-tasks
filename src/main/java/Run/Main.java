ab8fbd15fa54c180a3a2da79038734533556a1f2
Paweł Knapik <btc159357@gmail.com>
2024-05-28 20:59:05 +0200

    Tasks from 28.05.2024 Java

--- src/main/java/Run/Main.java
+++ src/main/java/Run/Main.java
@@ -1,10 +1,50 @@
 package Run;
 
-import Tasks.ReverseMode_1.*;
+
+import java.io.IOException;
+
 
 public class Main {
-    public static void main(String[] args) {
-       ReverseMode_11.task();
 
+
+    public static void main(String[] args) throws IOException {
+//        Scanner f = new Scanner(new FileInputStream("C:/Users/PC/Desktop/Nowy Dokument tekstowy.txt"));
+//        while (f.hasNext()) {
+
+//            System.out.println(f.next());
+//        }
+
+        System.out.println(aaa(1,55));
+        System.out.println(aaa(55,1));
+        System.out.println(aaa(7,13));
+        System.out.println(aaa(7,14));
+    }
+
+        public static int[][][] createCube(){
+            // write your code here
+            int[][][] arr = new int[3][3][3];
+            int a = 0;
+            for (int i = 0; i < 3; i++) {
+
+                for (int z = 0; z < 3; z++) {
+
+                    for (int c = 0; c < 3; c++) {
+                        arr[i][z][c] = a;System.out.println(a);
+                        a++;
+
+                    }
+                }
+           a = 0;
+            }
+
+
+            return arr;
+        }
+
+
+
+    public static boolean aaa(int a, int b){
+        return (a+b+(a*b)) == 111;
     }
 }
+
--- src/main/java/Tasks/FastestMode1/FM.java
+++ src/main/java/Tasks/FastestMode1/FM.java
@@ -1,32 +1,81 @@
 package Tasks.FastestMode1;
 
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class FM {
 
-    /**
-     * Auto-generated code below aims at helping you parse
-     * the standard input according to the problem statement.
-     **/
 
-        public void task() {
-            Scanner in = new Scanner(System.in);
-            StringBuilder n = new StringBuilder(in.nextLine());
+class Result {
 
-            char[] arr = n.toString().toCharArray();
-            n = new StringBuilder();
-            for (char x : arr) {
-                if (x % 2 == 0) {
-                    n.append("#");
-                } else {
-                    n.append("*");
-                }
-            }
+    public static void main(String[] args) {
 
-            // Write an answer using System.out.println()
-            // To debug: System.err.println("Debug messages...");
+        List aa = new ArrayList();
 
-            System.out.println(n);
-        }
+        aa.add(3);
+        aa.add(2);
+        aa.add(3);
+        aa.add(5);
+        System.out.println(getMaxValue(aa));
     }
 
+    /*
+     * Complete the 'getMaxValue' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+Scanner sc = new Scanner(System.in);
+    public static int getMaxValue(List<Integer> arr) {
+        // Write your code here
+
+        Collections.sort(arr);
+        long[] longNumbers = new long[]{100000000001L, 100000000002L, 100000000003L};
+
+        while (arr.get(0) > 1){
+            for (int i = 0; i< arr.size(); i++){
+                arr.set(i, arr.get(i)-1);
+            }
+        }
+
+        return arr.get(arr.size()-1);
+
+    }
+
+}
+
+ class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.getMaxValue(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
